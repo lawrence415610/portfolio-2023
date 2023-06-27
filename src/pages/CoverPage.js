ace1c46a54404ed122e46e0acc0c7c8ac0ea6a52
Lawrence <lawrence415610@gmail.com>
2023-06-28 09:25:11 +1000

    add a video cover

--- src/pages/CoverPage.js
+++ src/pages/CoverPage.js
@@ -1,21 +1,13 @@
 import React from "react";
-
 import "./CoverPage.scss";
 
 const CoverPage = () => {
 
   return (
     <section className="cover">
-      <h1 className="cover__title">
-        <span className="cover__name">
-          <span className="cover__name--left">Yelin</span>
-          <span className="cover__name--right">Liu</span>
-        </span>
-        <span className="cover__job">
-          <span className="cover__job--left">I'm a </span>
-          <span className="cover__job--right">creative developer</span>
-        </span>
-      </h1>
+      <video className="cover__video" autoPlay muted loop>
+		<source src='/video/websiteVedio.mp4' type="video/mp4" />
+	  </video>
     </section>
   );
 };
