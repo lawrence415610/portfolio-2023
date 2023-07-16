d557de364a4948d0a22a2b798a9a9dfde79c2f64
lawrence415610 <34848993+lawrence415610@users.noreply.github.com>
2023-07-16 23:19:14 +1000

    update Big text

--- src/pages/PortfolioPage.js
+++ src/pages/PortfolioPage.js
@@ -5,12 +5,14 @@ import ContainerLeft from "../layout/ContainerLeft";
 import ContainerRight from "../layout/ContainerRight";
 import Container from "../layout/Container";
 import PortfolioCards from "../components/PortfolioCards";
+import TitleBig from "../UI/TitleBig";
 //TODO: use GraphQL to fetch Github commit data and build heatmap myself
 const PortfolioPage = () => {
 	return (
 		<section className="section--portfolio">
 			<Container className="portfolio">
 				<ContainerLeft>
+					<TitleBig>My Portfolio</TitleBig>
 					<TitleSmall>Demo Projects</TitleSmall>
 					<PortfolioCards />
 				</ContainerLeft>
